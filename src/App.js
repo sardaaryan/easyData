e29d0560335733bd1d16816552d7bc25b467e8e6
Aryan Sarda <sardaaryan1@gmail.com>
2024-04-27 18:37:38 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import SearchIcon from "./search.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  /*const[elementOne, setElementOne] = useState("Poverty");*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Enter values to find their correlation</h1>
+      <div className="sub">
+        <input type="text" placeholder="Topic 1..." />
+        <input type="text" placeholder="Topic 2..." />
+      </div>
+      <img className="search" src={SearchIcon} alt="search" />
     </div>
   );
-}
+};
 
 export default App;
